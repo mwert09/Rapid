beb7e8734199d55aa4f6fb7200474767200134f3
mwert09 <mwert09@gmail.com>
2020-10-28 15:47:31 +0300

    Depth buffer and indexed draw

--- Rapid/main.cpp
+++ Rapid/main.cpp
@@ -18,7 +18,7 @@
 using namespace Rapid;
 using namespace Graphics;
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel;
 const float toRadians = 3.14159265f / 180.0f;
 
 bool direction = true;
@@ -39,34 +39,52 @@ static const char* vShader = "												\n\
 																			\n\
 layout (location = 0) in vec3 pos;											\n\
 																			\n\
+out vec4 vCol;																\n\
+																			\n\
 uniform mat4 model;															\n\
 																			\n\
 void main()																	\n\
 {																			\n\
 	gl_Position = model * vec4(pos, 1.0);									\n\
+	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);								\n\
+																			\n\
 }";
 
 // Fragment Shader
 static const char* fShader = "										\n\
 #version 330														\n\
 																	\n\
+in vec4 vCol;														\n\
+																	\n\
 out	vec4 colour;													\n\
 																	\n\
 void main()															\n\
 {																	\n\
-	colour = vec4(1.0f, 0.0f, 0.0f, 1.0);							\n\
+	colour = vCol;													\n\
 }";
 
 void CreateTriangle()
 {
+	unsigned int indices[] = {
+		0,3,1,
+		1,3,2,
+		2,3,0,
+		0,1,2
+	};
+	
 	GLfloat vertices[] = {
 		-1.0f, -1.0f, 0.0f,
+		0.0f, -1.0f, 1.0f,
 		1.0f, -1.0f, 0.0f,
 		0.0f, 1.0f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &IBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
@@ -76,8 +94,10 @@ void CreateTriangle()
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	
 	glBindVertexArray(0);
+	
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
@@ -147,6 +167,8 @@ int main()
 {
 	Window window("Rapid", 800, 600);
 
+	glEnable(GL_DEPTH_TEST);
+	
 	CreateTriangle();
 	CompileShaders();
 	
@@ -192,14 +214,16 @@ int main()
 		
 		glm::mat4 model = glm::mat4(1.0f);
 
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(curSize, 0.4f, 1.0f));
+		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 		
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 		glUseProgram(0);
 		window.Update();
 	}
